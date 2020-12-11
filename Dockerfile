f8eef82c9b9d512b631682603ecbefa7562d6024
Danila Grobov <danila.grob@gmail.com>
2020-12-11 13:45:22 +0000

    Setup docker for Express.js server.

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,10 @@
 FROM node:14.15.1-alpine3.10
+WORKDIR app/
+EXPOSE 3000:3000
+COPY backend/app.js app.js
+COPY package.json package.json
+COPY package-lock.json package-lock.json
+COPY babel.config.json babel.config.json
+RUN npm install
+RUN npm run build
+CMD node app.js
--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+import express from "express"
 const app = express()
 const port = 3000
 
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`The app is listening at http://localhost`)
 })
