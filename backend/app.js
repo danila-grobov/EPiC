09d1c67ea6e70a0511bb1a77a98b7ec3a7b16491
Danila Grobov <danila.grob@gmail.com>
2020-12-14 22:12:01 +0000

    Write readme file and add comment the code and config files.

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,14 @@ const dbCredentials = {
     port: '33060'
 };
 
+// Expose a folder to the web, this folder will be accessible from outside.
 app.use(express.static(path.join(__dirname, '../dist')));
-console.log(path.join(__dirname, '../dist'));
+// Setup a render engine to be used on .html files
+// Ejs allows us to make page templates with js
 app.engine('.html', require('ejs').__express);
+// Specify a folder, where the views will be stored
 app.set('views', path.join(__dirname, '../frontend/views'));
+// Provide an extension to use for views, so we won't have to specify it when rendering.
 app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
--- server_config/webpack.config.js
+++ server_config/webpack.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
-const webpack = require("webpack");
-
 module.exports = {
+    //The file to start interpretation from.
     entry: "./frontend/src/index.js",
     mode: "development",
-    watchOptions: { poll: true },
+    watchOptions: { poll: true }, //This is required in order for auto-reload to work in docker.
     module: {
         rules: [
             {
+                //Convert all the ES6 and jsx syntax into regular javascript.
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: "babel-loader",
@@ -16,12 +16,15 @@ module.exports = {
                 }
             },
             {
+                //Convert all style files to regular css and then insert it into bundle.
                 test:/\.s[ac]ss$/i,
                 use: ["style-loader", "css-loader", "sass-loader"]
             }
         ]
     },
     resolve: {
+        //Extensions that can be used as modules
+        //import "file.js" is an example of .js
         extensions: ["*", ".js", ".jsx"]
     },
     output: {
