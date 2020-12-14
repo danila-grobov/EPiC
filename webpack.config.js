c3b96237fecf9525f895f20c57c99b0b52734186
jacob <b9016184@newcastle.ac.uk>
2020-12-14 15:46:28 +0000

    add SCSS config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const ScssConfigWebpackPlugin = require('scss-config-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
@@ -33,5 +34,9 @@ module.exports = {
         publicPath: "http://localhost:3000/dist/",
         hotOnly: true
     },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new ScssConfigWebpackPlugin()
+    ]
 };
+
