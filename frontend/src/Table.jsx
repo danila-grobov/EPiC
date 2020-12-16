cce1421332d523fc2a752102ca67c863f8a42ae5
Danila Grobov <danila.grob@gmail.com>
2020-12-16 16:03:19 +0000

    Create visual part of table's search area.

--- frontend/src/Table.jsx
+++ frontend/src/Table.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 import "./table.scss"
+import SearchArea from "./SearchArea";
 const Table = () => {
     return (
         <div className="table">
-
+            <SearchArea />
         </div>
     )
 }
