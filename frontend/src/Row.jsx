1a75519004743963ce53bf1c59796be9151976ad
Danila Grobov <danila.grob@gmail.com>
2020-12-17 14:31:32 +0000

    Reduce title sizes for table columns.

--- frontend/src/Row.jsx
+++ frontend/src/Row.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import "./scss/row.scss"
 import checkBox from "./imgs/checkbox.svg"
-
 export default props => {
     const {values, rowType} = props;
     const cells = values.map(({value, type}, index) => {
