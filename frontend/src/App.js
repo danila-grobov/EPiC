dc5f43701b25bf0b221328d8c3b5abe621bc7af9
b9001346 <m.champagne@ncl.ac.uk>
2020-12-18 10:12:28 +0000

    Put in some framework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./app.scss";
+import Profile from "./Profile";
 
 class App extends Component {
     render() {
         return (
             <div className="app">
-                <h1> Hello, Dickhead! </h1>
+                <Profile/>
             </div>
         );
     }
