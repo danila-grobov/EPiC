016ee9f76ed5eae415bd6ecfd1eaa822cb6b63d6
Danila Grobov <danila.grob@gmail.com>
2020-12-24 13:47:36 +0200

    Add keys to resolve the warning.

--- frontend/src/table/Row.jsx
+++ frontend/src/table/Row.jsx
@@ -3,21 +3,22 @@ import "../scss/table/row.scss"
 import checkbox from "../imgs/checkbox.svg"
 import checkbox__checked from "../imgs/checkbox__checked.svg"
 import checkbox__partial from "../imgs/checkbox_partial.svg"
+
 export default props => {
-    const {values, rowType,tickCheckbox,selected,id} = props;
+    const {values, rowType, tickCheckbox, selected, id} = props;
     const cells = values.map(({value, type}, index) => {
-            const visibility = values.length-1 === index || type === "title" ? "--hidden" : "";
+            const visibility = values.length - 1 === index || type === "title" ? "--hidden" : "";
             return [
-                <span key={index} className={`row__cell row__cell--${type}`}>{value}</span>,
-                <div className={`row__cellSeparator${visibility}`}/>
+                <span key={"row_cell" + index} className={`row__cell row__cell--${type}`}>{value}</span>,
+                <div key={"row__cellSeparator" + index} className={`row__cellSeparator${visibility}`}/>
             ]
         }
     );
     const visibility = rowType === "header" ? "--hidden" : "";
     let icon = checkbox;
-    if(selected === "full")
+    if (selected === "full")
         icon = checkbox__checked;
-    else if(selected === "partial")
+    else if (selected === "partial")
         icon = checkbox__partial
     return (
         <div className={`row--${rowType}`}>
