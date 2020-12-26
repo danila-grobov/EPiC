32cbbd0874feeae3b4dcf17f1357d9ae74381827
Sofia E. Trevino <set888835@gmail.com>
2020-12-26 22:14:25 +0000

    Added content to nav bar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,20 @@ import NavBar from "./NavBar/NavBar";
 import NavBar_Dropdown from "./NavBar/NavBar_Dropdown";
 const App = () => {
     const course = "CSC2033";
+    const epic = "EPiC";
+    const pages = ["HOME", "TASKS", "MANAGE"];
+    const name = "Hello, Dwight";
+    const teacher = "TEACHER";
+    const admin = "ADMIN";
+
 
     return (
         <div className="app">
-            <NavBar course={course}/>
-            <Table/>
+            <NavBar course={course} epic={epic} pages={pages} name={name} teacher={teacher} admin{admin}/>
+            <Table/>;
         </div>
     );
 }
+
 
 export default App;
--- frontend/src/NavBar/NavBar.jsx
+++ frontend/src/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import "../scss/navBar.scss"
 import dropDownArrow from "../imgs/downArrow.svg"
+import profilePhoto from "../imgs/profilePhoto.svg"
 //controls all of the nav bar.
 
 //MenuBase = the entire nav bar
@@ -9,18 +10,44 @@ import dropDownArrow from "../imgs/downArrow.svg"
 //a toggle button
 
 export default (props) => {
-    const {course} = props;
+    const {course, epic, pages, name, teacher, admin} = props;
 
     return (
-       <div className="menuBase">
-           <div className = "navDropdown">
-               <span className="courseName">{course}</span>
-               <img src={dropDownArrow} alt="Dropdown icon" className="dropdown__icon"/>
-           </div>
-           <div className="navMainMenu">
-           </div>
+        <div className="menuBase">
+            <div className="navDropdown">
+                <span className="courseName">{course}</span>
+                <img src={dropDownArrow} alt="Dropdown icon" className="dropdown__icon"/>
+            </div>
 
-       </div>
+
+
+            <div className="navMainMenu">
+
+                <span className="epicLogo"> {epic}</span>
+
+                <div className="innerMenu">
+                    <div className= "pages">
+                        {pages.map((page) =>
+                            <span className="pageName">{page}</span>
+                        )}
+                    </div>
+
+                    <div className="separator"/>
+
+                    <span className="userName">{name}</span>
+                    <img src={profilePhoto} alt="Profile photo" className="profile__icon"/>
+
+                    <div className="toggleButton">
+                        <span className="teacher">{teacher}</span>
+                        <span className="admin">{admin}</span>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
     )
 
 }
