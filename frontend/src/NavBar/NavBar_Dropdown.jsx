315ddd353d1f068c6e844f5bbc8e886cb7448ac4
Sofia E. Trevino <set888835@gmail.com>
2021-01-01 21:07:42 +0000

    Minor fixes to li CSS in dropdown.scss.

--- frontend/src/NavBar/NavBar_Dropdown.jsx
+++ frontend/src/NavBar/NavBar_Dropdown.jsx
@@ -19,13 +19,13 @@ export default (props) => {
             <button className="dropbtn">COURSES</button>
 
             <div id= "dropdownContent" className="dropdown-content">
-                <ul>
+
                     <li><a href="#" >CSC2031</a></li>
                     <li><a href="#" >CSC2032</a></li>
                     <li><a href="#" >CSC2033</a></li>
                     <li><a href="#" >CSC2034</a></li>
                     <li><a href="#" >CSC2035</a></li>
-                </ul>
+
             </div>
             <button className="dropbtn-2"><img src={dropDownArrow} alt="Dropdown icon" className="dropdown__icon"/></button>
             </div>
