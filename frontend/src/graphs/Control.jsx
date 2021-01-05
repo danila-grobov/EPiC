1ec50cc32380a6a5b35557bd7503200c269fb0c5
jacob <b9016184@newcastle.ac.uk>
2021-01-05 15:12:23 +0000

    Control component updated. Comments added to Options

--- frontend/src/graphs/Control.jsx
+++ frontend/src/graphs/Control.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import BarGraph from "./BarGraph";
 import Options from "./Options";
+import Button from "../general_components/Button";
 
 // Create canvas for graph to be displayed
 // Choose graph to display, with any filters
@@ -27,7 +28,8 @@ export default () => {
             <Options />
 
 
-
+            {/*Get Button from general_components to re-render once filters selected*/}
+            <Button label={"Re-Render"} />
         </div>
     )
 }
--- frontend/src/graphs/Options.jsx
+++ frontend/src/graphs/Options.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import Input from "../general_components/Input";
 
 //Display and allow input of selected filters
 
@@ -9,6 +10,16 @@ export default () => {
         <div>
             <h4>Filters and Options</h4><br/>
 
+            {/* Drop down menu to choose graph
+            - Bar - Raw Grade (bounded)
+            - Bar - Number of tasks completed in a period
+            - Bar - Current confidence (bounded)
+            - Bar - Grade distribution
+            - Scatter - Confidence over a period
+            - Scatter - Confidence V.S. Grade
+            - Line - Average confidence over a period
+            - Pie - Confidence for a specified date
+            */}
         </div>
     )
 }
