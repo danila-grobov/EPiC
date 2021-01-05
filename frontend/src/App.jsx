474a560ae39967ba8b5e332dd7798875206c7f6e
jacob <b9016184@newcastle.ac.uk>
2021-01-05 12:01:20 +0000

    Graph control component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,14 @@ import "./scss/app.scss";
 import Table from "./table/Table";
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import BarGraph from "./graphs/BarGraph";
-import LineGraph from "./graphs/LineGraph";
+import Control from "./graphs/Control";
 // App stores and displays all of the top level components in the page.
 // ToastContainer is need to display alert messages.
 const App = () => {
     return (
         <div className="app">
             {/*{<Table course={"CSC2033"}/>}*/}
-            <LineGraph />
+            <Control/>
             <ToastContainer />
         </div>
     );
---
+++ frontend/src/graphs/Control.jsx
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from "react";
+import BarGraph from "./BarGraph";
+
+// Create canvas for graph to be displayed
+// Choose graph to display, with any filters
+// Call relevant graph component passing data
+
+export default () => {
+
+
+    return (
+        <div>
+            <h2>Here's a graph</h2><br/>
+            <BarGraph /* Pass data and filters */ /><br/>
+
+
+
+        </div>
+    )
+}
