ef07e2b0078caa293596b0fbd0329d6f9083086d
Danila Grobov <danila.grob@gmail.com>
2021-01-08 15:16:58 +0200

    Create basic dashboard structure and course cards.

---
+++ frontend/src/dashboard/Dashboard.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import CourseCard from "./CourseCard";
+import "../scss/app.scss";
+import "../scss/dashboard/dashboard.scss";
+export default () => {
+    const courses = [
+        {
+            name: "CSC2031",
+            color: "#F28F38",
+            progress: 25
+        },
+        {
+            name: "CSC2032",
+            color: "#F28F38",
+            progress: 50
+        },
+        {
+            name: "CSC2033",
+            color: "#7A306C",
+            progress: 12,
+        },
+        {
+            name: "CSC2034",
+            color: "#C8553D",
+            progress: 0
+        },
+        {
+            name: "CSC2035",
+            color: "#7A306C",
+            progress: 99
+        }
+    ]
+    return (
+        <div className="dashboard">
+            <div className="dashboard__courseCards">
+                {courses.map(course =>
+                    <CourseCard {...course}/>
+                )}
+            </div>
+        </div>
+    )
+}
