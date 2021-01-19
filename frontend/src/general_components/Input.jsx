9fb5f7423281d3a14525b209b906af4f46478428
Danila Grobov <danila.grob@gmail.com>
2021-01-19 18:08:18 +0200

    Reset filter input on submit.

--- frontend/src/general_components/Input.jsx
+++ frontend/src/general_components/Input.jsx
@@ -4,9 +4,13 @@ import useValue from "../hooks/useValue";
 
 export default props => {
     const {className, onSubmit, charLimit, listeners} = props;
-    const {bind, value} = useValue("");
+    const {bind, value, setValue} = useValue("");
     return (
-        <form onSubmit={e => {e.preventDefault();onSubmit(value);}}>
+        <form onSubmit={e => {
+            e.preventDefault();
+            onSubmit(value);
+            setValue("");
+        }}>
             <input type="text" maxLength={charLimit} className={className} {...bind} {...listeners}/>
         </form>
     );
--- frontend/src/modified_axios.js
+++ frontend/src/modified_axios.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 axios.interceptors.response.use(res => {
+    console.log(res.status);
     return res;
 }, error => {
-    const {status, data} = error.response;
-    if(status === 303) {
-        window.location = data;
+    if(error.response) {
+        const {status, data} = error.response;
+        if(status === 303) {
+            window.location = data;
+        }
     }
+    return Promise.reject();
 })
 export default axios;
