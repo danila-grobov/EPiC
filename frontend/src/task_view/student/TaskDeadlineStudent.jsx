c095c780d2937b507fc798c7b76fe014a131bf9e
B9014345 <b9014345@newcastle.ac.uk>
2021-01-22 13:27:41 +0000

    Started comments.

--- frontend/src/task_view/student/TaskDeadlineStudent.jsx
+++ frontend/src/task_view/student/TaskDeadlineStudent.jsx
@@ -1,6 +1,9 @@
+/**
+ * @author Erikas Nakonecnikovas
+ */
 import React from 'react';
 import '../../scss/taskDeadline.scss'
 
 export default (taskDeadline) => {
-    return <p id="task-deadline">{taskDeadline.taskDeadline !== null || taskDeadline.taskDeadline !== "" ? taskDeadline.taskDeadline : ""}</p>
+    return <p id="task-deadline">{taskDeadline.taskDeadline !== "Invalid date" ? taskDeadline.taskDeadline : ""}</p>
 }
